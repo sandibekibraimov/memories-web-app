87c15dbab19301ebd9feee5c25377f2c0439d247
sandibekibraimov <sandibek@gmail.com>
2021-09-25 12:14:01 -0400

    styles added

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -1,9 +1,27 @@
+import { response } from 'express';
 import Post from '../models/Post.js';
 
-export const getPosts = (req, res) => {
-  res.send('work');
+// getting all posts route
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ msg: error.message });
+  }
 };
 
-export const createPost = (req, res) => {
-  res.send('created');
+// creating new post route
+export const createPost = async (req, res) => {
+  const { post } = req.body;
+
+  const newPost = new Post(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ msg: error.message });
+  }
 };
